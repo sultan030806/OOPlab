fa9bd73ec2bd31f3902cf2b274310c5c9e4c1247
sultan030806 <sultan030806@gmail.com>
2025-12-25 20:17:12 +0500

    Update CircleWidget.h

--- oop3.1/CircleWidget.h
+++ oop3.1/CircleWidget.h
@@ -3,38 +3,47 @@
 #include <QWidget>
 #include <QPainter>
 #include <QMouseEvent>
+#include <QKeyEvent>
+#include <QMainWindow>
+#include <QLabel>
+#include <QStatusBar>
+#include <QApplication>
 #include <vector>
+#include <memory>
 
+// Класс круга
 class Circle {
 public:
-    static const int RADIUS = 20;
+    static const int RADIUS = 20; // Постоянный радиус для всех кругов
 
     Circle(int x, int y);
+
     void draw(QPainter& painter) const;
     bool contains(int x, int y) const;
+    bool isSelected() const { return selected; }
+    void setSelected(bool sel) { selected = sel; }
     int getX() const { return x; }
     int getY() const { return y; }
 
-bool isSelected() const { return selected; }
-    void setSelected(bool sel) { selected = sel; }
-    
 private:
     int x, y;
-    bool selected = false;  // Добавлено
+    bool selected;
 };
 
-class CircleStorage {  // Новый класс
+// Кастомный контейнер для хранения кругов
+class CircleStorage {
 public:
     void addCircle(std::shared_ptr<Circle> circle);
     void clearSelection();
+    void removeSelected();
     std::vector<std::shared_ptr<Circle>>& getCircles() { return circles; }
-    
+    const std::vector<std::shared_ptr<Circle>>& getCircles() const { return circles; }
+
 private:
     std::vector<std::shared_ptr<Circle>> circles;
 };
 
-class CircleWidget : public QWidget {
-
+// Виджет-холст для отрисовки кругов
 class CircleWidget : public QWidget {
     Q_OBJECT
 
@@ -44,7 +53,20 @@ public:
 protected:
     void paintEvent(QPaintEvent* event) override;
     void mousePressEvent(QMouseEvent* event) override;
+    void keyPressEvent(QKeyEvent* event) override;
+    void resizeEvent(QResizeEvent* event) override;
 
 private:
-    CircleStorage storage;  // Заменили vector на CircleStorage
+    CircleStorage storage;
+
+    // Для вывода сообщений в GUI вместо консоли
+    void showMessage(const QString& message);
+};
+
+// Главное окно приложения
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = nullptr);
 };
