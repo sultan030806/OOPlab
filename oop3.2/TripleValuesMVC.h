e9b1565f88729485a76d5a7433a9b63bf88f995c
sultan030806 <sultan030806@gmail.com>
2025-12-25 21:53:21 +0500

    Create TripleValuesMVC.h

---
+++ oop3.2/TripleValuesMVC.h
@@ -0,0 +1,62 @@
+#ifndef TRIPLEVALUESMVC_H
+#define TRIPLEVALUESMVC_H
+
+#include <QMainWindow>
+#include <QObject>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QSpinBox>
+
+// Простейшая модель
+class TripleValueModel : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit TripleValueModel(QObject* parent = nullptr);
+
+    int valueA() const { return m_valueA; }
+    int valueB() const { return m_valueB; }
+    int valueC() const { return m_valueC; }
+
+    void setValueA(int value);
+    void setValueB(int value);
+    void setValueC(int value);
+
+signals:
+    void valuesChanged(int a, int b, int c);
+
+private:
+    void ensureConsistency();
+
+    int m_valueA;
+    int m_valueB;
+    int m_valueC;
+    const int m_minValue = 0;
+    const int m_maxValue = 100;
+};
+
+// Главное окно с минимальным UI
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = nullptr);
+
+private slots:
+    void onModelValuesChanged(int a, int b, int c);
+    void onSpinAChanged(int value);
+    void onSpinBChanged(int value);
+    void onSpinCChanged(int value);
+
+private:
+    void setupUI();
+    
+    TripleValueModel* m_model;
+    QSpinBox* m_spinBoxA;
+    QSpinBox* m_spinBoxB;
+    QSpinBox* m_spinBoxC;
+};
+
+#endif // TRIPLEVALUESMVC_H
